179a2b3e48cfcbb0f78693de8b74dbc9e928d0f2
Tamjid <tkhan314@ucla.edu>
2022-10-18 15:09:55 -0700

    Completed pipe.c with passing test cases

--- pipe.c
+++ pipe.c
@@ -6,40 +6,59 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+extern int errno;
+
 int main(int argc, char *argv[])
 {
-	
+	int status;
 	for (int i = 1; i < argc; i++)
 	{
 		int fd[2]; //Pipefd[0] - read end,  Pipefd[1] - write end
+		int exit[2]; // Use a seperate pipe to send child exit status to parent
+		if (pipe(exit) == -1) //Open pipe and catch error
+		{
+			perror("Error: unable to open pipe");
+			return errno;
+		}
 		if (pipe(fd) == -1) //Open pipe and catch error
 		{
 			perror("Error: unable to open pipe");
-			exit(EXIT_FAILURE);
+			return errno;
 		}
 		int pid = fork();
 		if (pid == -1) //Create fork and catch error
 		{
 			perror("Error: unable to fork processes");
-			exit(EXIT_FAILURE);
+			return errno;
 		}
 
 		if (pid == 0) //Child Process that runs argument
 		{
+			close(exit[0]);
 			if (i+1 < argc) //Check if there is an argument in front
 			{
 				dup2(fd[1], 1);// Redirect stdout to write end of pipe
-				close(fd[0]);
+				close(fd[0]); //Close read end of the pipe
 			}
 
-			execlp(argv[i], argv[i], NULL);
-			exit(0);
+			if (execlp(argv[i], argv[i], NULL) == -1)
+			{
+				status = EINVAL;
+				write(exit[1], &status, sizeof(status));
+			}
 		}
-		else //Parent process that waits.
+		else //Parent process that waits for child process.
 		{
+			int stat = 0; // stat stores status from pipe exit[]
+			close(exit[1]);
 			dup2(fd[0], 0); // Redirect stdin to read end of pipe
-			close(fd[1]);
-			waitpid(pid, NULL, 0);
+			close(fd[1]); // CLose the write end of the pipe
+			read(exit[0], &stat, sizeof(stat));
+			if (stat == EINVAL) //Check if the status from the child is EINVAL
+			{
+				return stat;
+			}
+			waitpid(pid, &status, 0);
 		}
 
 		close(fd[0]);
